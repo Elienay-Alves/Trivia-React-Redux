d43b41616d113ba35285350ca61077cfa83f1212
Renata Dias <renata@pop-os.localdomain>
2022-04-07 17:06:34 -0300

    conflito gitHub

--- src/services/fetchGravatar.js
+++ src/services/fetchGravatar.js
@@ -1,5 +1,5 @@
 const fetchGravatar = async (email) => {
-  const response = await fetch(`https://www.gravatar.com/avatar/${email}?d=wavatar`);
+  const response = await fetch(`https://www.gravatar.com/avatar/${email}`);
   return response;
 };
 
