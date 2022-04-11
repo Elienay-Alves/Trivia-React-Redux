8493d766a82459101f2adeccec712479a6f3b2a8
Renata Dias <renata@pop-os.localdomain>
2022-04-11 14:58:42 -0300

    atualização da branch

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,82 +3,53 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { actionFetchQuestion } from '../redux/action';
 import './buttonColor.css';
-import Cronometer from './Cronometer';
-
-const CORRECT_ANSWER = 'correct-answer';
 
 class Question extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      ind: 0,
-      answered: false,
       indice: 0,
-      isBtnDisabled: false,
-      nextBtn: false,
     };
   }
 
   componentDidMount() {
     const { fetchQuestions } = this.props;
     fetchQuestions();
-    this.validationSecond();
   }
 
-  getButtonColor = () => {
-    this.setState(() => ({ answered: true }), this.validationNext);
-  }
-
-  nextQuestion = () => {
+  getButtonColor = ({ target }, incAnswer, quest, index) => {
     const seconds = 500;
+    const verifyAnswer = this.verifiAnswer(incAnswer, quest, index);
+
+    if (verifyAnswer === 'correct-answer') {
+      target.className = verifyAnswer;
+    } else {
+      target.className = 'wrong-answer';
+    }
+
     setTimeout(() => this.setState((prevState) => ({
       indice: prevState.ind + 1,
     })), seconds);
-    this.setState((prevState) => ({ ind: prevState.ind + 1 }));
-    this.setState(() => ({ answered: false }));
-  };
+  }
 
   allAnswers = (incAnswers, corAnswer) => {
     const numberRandom = 0.5;
-    const quests = incAnswers.map((answer) => {
-      const objAnswer = {
-        answer,
-        type: 'wrong-answer',
-      };
-      return objAnswer;
-    });
-    quests.push({
-      answer: corAnswer,
-      type: CORRECT_ANSWER,
-    });
+    const quests = [...incAnswers, corAnswer];
     const shuffle = quests.sort(() => Math.random() - numberRandom);
     return (shuffle);
   }
 
-  verifiAnswer = (answer, index) => {
-    if (answer === CORRECT_ANSWER) {
+  verifiAnswer = (answer, corAnswer, index) => {
+    if (answer === corAnswer) {
       return 'correct-answer';
     }
     return `wrong-answer-${index}`;
   }
 
-  validationSecond = () => {
-    const trintaMil = 30000;
-    setTimeout(() => this.setState({
-      isBtnDisabled: true,
-    }), trintaMil);
-  }
-
-  validationNext = () => {
-    this.setState({
-      nextBtn: true,
-    });
-  }
-
   render() {
     const { questions } = this.props;
-    const { indice, answered, isBtnDisabled, nextBtn } = this.state;
+    const { indice } = this.state;
     return (
       <>
         {
@@ -86,40 +57,23 @@ class Question extends React.Component {
             <div key={ question.question }>
               <h2 data-testid="question-category">{ question.category }</h2>
               <h3 data-testid="question-text">{ question.question }</h3>
-              <div
-                data-testid="answer-options"
-              >
-                {
-                  this.allAnswers(question.incorrect_answers, question.correct_answer)
-                    .map((incAnswer, index) => (
-                      <button
-                        key={ incAnswer.answer }
-                        className={ answered ? incAnswer.type : null }
-                        type="button"
-                        data-testid={ this.verifiAnswer(incAnswer.type, index) }
-                        onClick={ () => this.getButtonColor() }
-                        disabled={ isBtnDisabled }
-                      >
-                        { incAnswer.answer }
-                      </button>
-                    ))
-                }
-                <Cronometer />
-              </div>
-              {
-                nextBtn
-                  ? (
+              <div data-testid="answer-options">
+                { this.allAnswers(question.incorrect_answers, question.correct_answer)
+                  .map((incAnswer, index) => (
                     <button
+                      key={ incAnswer }
+                      className=""
                       type="button"
-                      onClick={ this.nextQuestion }
-                      data-testid="btn-next"
+                      onClick={ (event) => (this.getButtonColor(event, incAnswer,
+                        question.correct_answer, index)) }
+                      data-testid={
+                        this.verifiAnswer(incAnswer, question.correct_answer, index)
+                      }
                     >
-                      Próximo
-
+                      { incAnswer }
                     </button>
-                  )
-                  : ''
-              }
+                  ))}
+              </div>
             </div>
           ))
         }
@@ -127,15 +81,19 @@ class Question extends React.Component {
     );
   }
 }
+
 Question.propTypes = {
   token: PropTypes.string,
   questions: PropTypes.arrayOf(PropTypes.object),
 }.isRequired;
+
 const mapStateToProps = (state) => ({
   token: state.token,
   questions: state.questions,
 });
+
 const mapDispatchToProps = (dispatch) => ({
   fetchQuestions: () => dispatch(actionFetchQuestion()),
 });
+
 export default connect(mapStateToProps, mapDispatchToProps)(Question);
